65d0e9c0ae09938b234e40fed1e8ad1ea2c5eda4
MrHidalgo <Luckyhidalgo@gmail.com>
2018-10-29 17:43:09 +0200

    Initial commit

---
+++ dest/js/app.js
@@ -0,0 +1,87 @@
+"use strict";
+
+/*
+*
+* ============================
+* ============================
+*
+* Include lib:
+*
+* - webFontLoader.js;
+* - preventBehavior.js;
+* - svg4everybody.js;
+*
+* ============================
+* ============================
+* */
+
+/**
+ * @name initPreventBehavior
+ * @description
+ */
+var initPreventBehavior = function initPreventBehavior() {
+  var link = document.querySelectorAll("a");
+
+  link.forEach(function (val, idx) {
+
+    val.addEventListener("click", function (e) {
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
+};
+
+/**
+ * @name initSvg4everybody()
+ * @description SVG for Everybody adds external spritemaps support to otherwise SVG-capable browsers.
+ */
+var initSvg4everybody = function initSvg4everybody() {
+
+  svg4everybody();
+};
+
+/**
+ * @name initWebFontLoader
+ * @description Loading fonts regardless of the source, then adds a standard set of events you may use to control the loading experience... for more details => https://github.com/typekit/fvd
+ */
+var initWebFontLoader = function initWebFontLoader() {
+
+  // WebFont.load({
+  //   google: {
+  //     families: [
+  //       'Roboto:100,300,400,500,600,700,900'
+  //     ]
+  //   }
+  // });
+
+  var WebFontConfig = {
+    custom: {
+      families: ['ProximaNova:n3,n4,n5,n6,n7,n8,n9']
+    }
+  };
+};
+
+/**
+ * @description Document DOM ready.
+ */
+(function () {
+  /**
+   * =============================================
+   * CALLBACK
+   * =============================================
+   */
+
+  /**
+   * @description Init all method
+   */
+  var initNative = function initNative() {
+    // default
+    initWebFontLoader();
+    initPreventBehavior();
+    initSvg4everybody();
+    // lib
+    // callback
+  };
+  initNative();
+})();
