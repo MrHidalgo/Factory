625dbad7ff5d8cd343482a60792aff8f93c2f07a
MrHidalgo <Luckyhidalgo@gmail.com>
2018-12-20 19:51:15 +0200

    Gulp pug - comments emmit stream.

--- gulp/task/pug.js
+++ gulp/task/pug.js
@@ -3,9 +3,7 @@ const gulp        = require('gulp'),
 	pug             = require('gulp-pug'),
 	plumber         = require('gulp-plumber'),
 	frontMatter     = require('gulp-front-matter'),
-	emitty          = require('emitty').setup('src/pug', 'pug', {
-		makeVinylFile: true
-	}),
+	emitty          = require('emitty').setup('src/pug', 'pug'),
 	htmlmin         = require('gulp-htmlmin');
 
 
@@ -23,56 +21,46 @@ const configPath  = require('../config/configPath'),
  */
 const srcPath = configPath.src.templates + '/*.pug';
 
-/**
- * @description Gulp PUG/JADE watch - keeps track of changes in files.
- */
-gulp.task('pug:watch', function() {
-	global.watch = true;
-
-	gulp.watch(configPath.src.templates + '/**', ['pug']).on('all', (event, filepath) => {
-		global.emittyChangedPugFile = filepath;
-	});
-});
-
 
 /**
  * @description Gulp PUG/JADE - preprocessor for creating html files.
  */
 const renderPug = () => {
-	const sourceOptions = global.watch ? { read: false } : {};
-
-	new Promise((resolve, reject) => {
-
-		const sourceOptions = global.watch ? { read: false } : {};
-
-		emitty.scan(global.emittyChangedPugFile).then(() => {
-			gulp
-				.src(srcPath, sourceOptions)
-				.pipe(plumber(configOption.pipeBreaking.err))
-				.pipe(gulpif(global.watch, emitty.filter(global.emittyChangedFile)))
-				.pipe(frontMatter({
-					property: 'data'
-				}))
-				.pipe(pug({
-					pretty: true,
-					data: {
-						env : (argv.prod) ? 'production' : ""
-					},
-				}))
-				.pipe(gulpif(argv.prod, htmlmin({
-					collapseBooleanAttributes: true,
-					collapseWhitespace: true,
-					removeEmptyAttributes: true,
-					removeComments: true
-				})))
-				.pipe(gulp.dest(configPath.dest.html))
-				.on('end', resolve)
-				.on('error', reject);
-		});
-	})
+	return gulp
+		.src(srcPath)
+		.pipe(plumber(configOption.pipeBreaking.err))
+		// .pipe(gulpif(global.isPugWatching, emitty.stream(global.emittyChangedPugFile)))
+		// .pipe(emitty.stream(global.emittyChangedPugFile))
+		.pipe(frontMatter({
+			property: 'data'
+		}))
+		.pipe(pug({
+			pretty: true,
+			data: {
+				env : (argv.prod) ? 'production' : ""
+			},
+		}))
+		.pipe(gulpif(argv.prod, htmlmin({
+			collapseBooleanAttributes: true,
+			collapseWhitespace: true,
+			removeEmptyAttributes: true,
+			removeComments: true
+		})))
+		.pipe(gulp.dest(configPath.dest.html));
 };
 
 gulp.task('pug', function() {
 	return renderPug();
 });
 
+
+/**
+ * @description Gulp PUG/JADE watch - keeps track of changes in files.
+ */
+gulp.task('pug:watch', function() {
+	// global.isPugWatching = true;
+
+	gulp.watch(configPath.src.templates + '/**', ['pug']).on('all', (event, filepath) => {
+		global.emittyChangedPugFile = filepath;
+	});
+});
